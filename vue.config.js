836ed79f042299ec2c00397164f9bed279f01844
SuperSonic <supersonic@livemail.tw>
2022-05-27 17:53:35 +0800

    Update publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  pages: {
-      index: {
-          title: 'Karafa',
-          entry: 'src/main.js',
-          template: 'public/index.html',
-          filename: 'index.html',
-      }
-  },
+    pages: {
+        index: {
+            title: 'Karafa',
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ? '/karafa/' : '/',
 };
